7497de92edcea94b0029d146a32605327b84dafc
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-10-13 20:45:26 +0100

    First and Second Day

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCore",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "476b7a6ab1mshd8113ca50851c82p1724adjsn0e2ea6aab191"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => "/charts/track",
+    }),
+    // getRecommendationsSongs: builder.query({
+    //   query: () => "/song/recommendations/",
+    // }),
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetRecommendationsSongsQuery } =
+  shazamCoreApi;
